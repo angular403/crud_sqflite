cd0e423df1c718ffc1ce0c0f6ad662d0f2afdb6b
angular403 <angularsprings403@gmail.com>
2022-03-11 04:07:56 +0700

    shared preferences

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,49 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  RxInt data = 0.obs;
 
-  final count = 0.obs;
+  void increment() {
+    data++;
+    simpanData();
+  }
+
+  void derement() {
+    data--;
+    simpanData();
+  }
+
+  void simpanData() async {
+    print("Simpan Data");
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getInt('angkaTerakhir') != null) {
+      await prefs.remove('angkaTerakhir');
+    }
+    await prefs.setInt('angkaTerakhir', data.value);
+  }
+
+  void bacaData() async {
+    print("Baca Data");
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getInt('angkaTerakhir') != null) {
+      data.value = prefs.getInt('angkaTerakhir')!;
+    }
+  }
+
+  void resetData() async {
+    print("Reset Data");
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getInt('angkaTerakhir') != null) {
+      await prefs.remove('angkaTerakhir');
+      data.value = 0;
+    }
+  }
+
   @override
   void onInit() {
+    // TODO: implement onInit
+    bacaData();
     super.onInit();
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,19 +3,67 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
+    // controller.bacaData();
     return Scaffold(
       appBar: AppBar(
         title: Text('HomeView'),
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.resetData();
+              },
+              icon: Icon(Icons.restore))
+        ],
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(() => Text(
+                  '${controller.data}',
+                  style: TextStyle(fontSize: 20),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(150, 50),
+                    primary: Colors.red[900],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  onPressed: () {
+                    controller.increment();
+                  },
+                  child: Text("+"),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(150, 50),
+                    primary: Colors.amber,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  onPressed: () {
+                    controller.derement();
+                  },
+                  child: Text("-"),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
